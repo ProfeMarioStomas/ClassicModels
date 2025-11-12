a1ab8f08f54b432077746221a6152772a8bc175a
Mario Cares <mariocaresca@santotomas.cl>
2025-11-12 07:52:14 -0300

    First upload

---
+++ src/main/java/cl/profemariostomas/classicmodels/ClassicModels.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.profemariostomas.classicmodels;
+
+/**
+ *
+ * @author luk0s
+ */
+public class ClassicModels {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
